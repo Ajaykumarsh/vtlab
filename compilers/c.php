a188ad81b5a4b404d7565ebedd443e91809f22c7
anush <anushdp.dsce@gmail.com>
2019-09-01 15:18:28 +0530

    HI

--- compilers/c.php
+++ compilers/c.php
@@ -1,8 +1,8 @@
 <?php
     
     putenv("PATH=/app/.apt/usr/bin:/app/.apt/usr/lib:/app/.apt/usr/include");
-	$CC="gcc";
-	exec("chmod a+x $CC"); 
+	$CC="gcc --sysroot=/app/.apt";
+	//exec("chmod a+x $CC"); 
 	$out="a.out";
 	$code=$_POST["code"];
 	$input=$_POST["input"];
