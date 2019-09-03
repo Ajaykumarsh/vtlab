5c73d147a0384ebbb580aecf0661192390263c1e
anush <anushdp.dsce@gmail.com>
2019-09-03 23:04:20 +0530

    HI

--- english.php
+++ english.php
@@ -4,9 +4,9 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css">
+  <script src="js/jquery-3.4.1.min.js"></script>
+  <link rel="stylesheet" href="css/Semantic/semantic.min.css">
   <script src="js/semantic.min.js"></script>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="js/script.js"></script>
   <link rel="stylesheet" href="css/home.css">
   <title>English</title>
