2e8ee57bf2a391e34e1bc5d24811f1b7007c73cb
anush <anushdp.dsce@gmail.com>
2019-09-05 21:07:35 +0530

    commit n

--- index.php
+++ index.php
@@ -13,11 +13,17 @@
         <title>Virtual-Labs</title>
 </head>
 <body>
-  <h1 class="ui header" style="text-align:center" id = "head">
-    <a class="ui circular image" href="http://dsce.edu.in"><img src="images/dscelogo.jpg"></a>
-  Virtual Labs 
-</h1>
-  <div>
+
+  <div class="ui header" style="text-align:center;" id ="head">
+  <a class="ui circular image" href="http://dsce.edu.in"><img src="images/dscelogo.jpg"></a>  <span style="color:#3053AB"> Dayananda Sagar College Of Engineering</span> 
+ 
+ <p style="font-size:18px;margin-top:-15px;margin-left:-10px;">Learning initiative by the Department of Computer Science</p>
+</div>
+<div class="ui header">
+<h1 style="text-align:center;font-size:40px;margin-left:-10px;">Virtual Tutorial Lab</h1>
+  </div>
+<div>
+  
   <div class="ui  light grey inverted secondary huge menu">
   <a class="header item" href="index.php">
     Home
@@ -46,7 +52,7 @@
        </div>
          <div class="content">
            <span class="CardBody">
-             Contains 6 Experiments On Data Structures in C Programming Language
+             Contains 12 Experiments On Data Structures in C Programming Language
            </span>
            <br><br>
            <a class="ui button" href="datastructures.php" >Go</a>
@@ -65,7 +71,7 @@
        </div>
          <div class="content">
            <span class="CardBody">
-             Contains 10 Experiments On Object Oriented Programming in Java
+             Contains 12 Experiments On Object Oriented Programming in Java
            </span>
            <br><br>
            <a class="ui button" href="OopsWithJava.php" >Go</a>
@@ -131,15 +137,54 @@
   
       </div>
       </div>
+
+      <div class="five wide column">
+        <div class="ui raised card">
+       <div class="content" id="Card5">
+      <div class="header">
+         <a href="english.php" >
+             Pre-Placement Training
+         </a>
+      </div>
+       </div>
+         <div class="content">
+           <span class="CardBody">
+             Contains something related to the Pre-placement stuffs
+           </span>
+           <br><br>
+           <a class="ui button" href="english.php">Go</a>
+         </div>
+  
+      </div>
+      </div>
       
     
   </div>
     </div>
-  <div class="ui inverted  large segment" id="footer">
-    <div class="foot">
-      <h1>
-        
-      </h1>
+  <div class="ui inverted  large segment" >
+    <div class="ui grid" style="" >
+      <div class="five wide column" style="text-align:right ;">
+      <h2 style="margin-top:15px;color:#365CCF;">
+       Under the Guidence of :
+      </h2>
+      <p style="color:#E9F035;">HOD of Computer Science Prof Ramesh Babu.</p>
+      <p style="margin-left:20px;color:#E9F035;">Prof Anitha </p>
+      <p style="margin-left:20px;color:#E9F035;">Prof Amutha</p>
+      <p></p>
+      </div>
+      <div class="five wide column">
+
+      </div>
+      <div class="five wide column" style="text-align:left">
+      
+      <h2 style=" margin-top:15px;color:#365CCF;">Student Co-ordinators:</h2>
+      <p style=" margin-left:15px;color:#E9F035;">Harshith R S</p>
+      <p style=" margin-left:15px;color:#E9F035;">HemanthKumar V</p>
+      <p style=" margin-left:15px;color:#E9F035;">Anush D P</p>
+      <p style=" margin-left:15px;color:#E9F035;">Anoop Satish Gejje</p>
+      <p style=" margin-left:15px;color:#E9F035;">Ajaykumar S Hadimani</p>
+      
+      </div>
 
     </div>
   </div>
