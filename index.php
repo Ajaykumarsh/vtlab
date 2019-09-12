bd1bd5d59ac4ef8b202e25613d28a6cf18fba62c
SerFuyah <hemanth18kumarv@gmail.com>
2019-09-12 14:33:34 +0530

    Final ig

--- index.php
+++ index.php
@@ -17,11 +17,8 @@
   <div class="ui header" style="text-align:center;" id ="head">
   <a class="ui circular image" href="http://dsce.edu.in"><img src="images/dscelogo.jpg"></a>  <span style="color:#3053AB"> Dayananda Sagar College Of Engineering</span> 
  
- <p style="font-size:18px;margin-top:-15px;margin-left:-10px;">Learning initiative by the Department of Computer Science</p>
+ <br><p style="font-size:25px;margin-top:-8px; text-align: center;">Learning initiative by the Department of Computer Science</p>
 </div>
-<div class="ui header">
-<h1 style="text-align:center;font-size:40px;margin-left:-10px;">Virtual Tutorial Lab</h1>
-  </div>
 <div>
 <div id="toTop"><i class="chevron up icon"></i></div>
   <div class="ui  light grey inverted secondary huge menu">
@@ -41,131 +38,40 @@
 </div>
 <div id="toTop"><i class="chevron up icon"></i></div>
 <div class="ui container" id="CardGrid">
-  <div class="ui stackable grid">
-    <div class="five wide column">
-      <div class="ui raised card" id="car1">
-       <div class="content" id="Card1">
-      <div class="header">
-            <a href="datastructures.php">
-             Data Structures Laboratory
-            </a></div>
-       </div>
-         <div class="content">
-           <span class="CardBody">
-             Contains 12 Experiments On Data Structures in C Programming Language
-           </span>
-           <br><br>
-           <a class="ui button" href="datastructures.php" >Go</a>
-         </div>
-      </div> 
-    </div>
-      
-    <div class="five wide column">
-        <div class="ui raised card" id="car2">
-       <div class="content" id="Card2">
-      <div class="header">
-         <a href="OopsWithJava.php" >
-             OOPS With Java Laboratory
-         </a>
-      </div>
-       </div>
-         <div class="content">
-           <span class="CardBody">
-             Contains 12 Experiments On Object Oriented Programming in Java
-           </span>
-           <br><br>
-           <a class="ui button" href="OopsWithJava.php" >Go</a>
-         </div>
-    </div>
-    </div>
-    <div class="five wide column">
-        <div class="ui  raised card" id="car3">
-       <div class="content" id="Card3">
-      <div class="header">
-         <a href="datastructures.php" >
-             Computer Networks Laboratory
-         </a>
-      </div>
-       </div>
-         <div class="content">
-           <span class="CardBody">
-             Contains 12 Experiments On Computer Networks in Cisco Packet Tracer
-           </span>
-           <br><br>
-           <a class="ui button" href="datastructures.php" >Go</a>
-         </div>
-  
-      </div>
-      </div>
-      
-      <div class="five wide column">
-        <div class="ui raised card" id="car4">
-       <div class="content" id="Card4">
-      <div class="header">
-         <a href="datastructures.php" >
-             Database Management Laboratory
-         </a>
-      </div>
-       </div>
-         <div class="content">
-           <span class="CardBody">
-             Contains 12 Experiments On Database Management in Oracle SQL
-           </span>
-           <br><br>
-           <a class="ui button" href="dbms.php" >Go</a>
-         </div>
-  
-      </div>
-      </div>
-  
-      <div class="five wide column">
-        <div class="ui raised card">
-       <div class="content" id="Card5">
-      <div class="header">
-         <a href="english.php" >
-             English Language
-         </a>
-      </div>
-       </div>
-         <div class="content">
-           <span class="CardBody">
-             Contains something related to the English Language
-           </span>
-           <br><br>
-           <a class="ui button" href="english.php">Go</a>
-         </div>
-  
-      </div>
-      </div>
 
-      <div class="five wide column">
-        <div class="ui raised card">
-       <div class="content" id="Card5">
-      <div class="header">
-         <a href="placement.php" >
-             Pre-Placement Training
-         </a>
-      </div>
-       </div>
-         <div class="content">
-           <span class="CardBody">
-             Contains something related to the Pre-placement stuffs
-           </span>
-           <br><br>
-           <a class="ui button" href="placement.php">Go</a>
-         </div>
-  
-      </div>
-      </div>
-      
-    
+<h1 style="text-align:center;font-size:40px;margin-left:-10px;">Virtual Tutorial Lab</h1><br><br>
+<span style="font-size:17px;">Virtual Tutorial Lab is an initiative taken by the <strong>Department of Computer Science Engineering</strong>, Dayananda Sagar Institutions to help students to get better at Laboratory programs as well as Placements. </span><br><br>
+
+  <div class="headerr">Vision of the Department</div><br><br>
+  <div style="font-size:17px;">To provide a vibrant learning environment in computer science and engineering with focus on industry needs and research, for the students to be successful global professionals contributing to the society.</div><br><br>
+
+  <div class="headerr">Mission of the Department</div><br>
+
+  <div style="font-size:17px;" class="ui bulleted list">
+  <span class="item">To adopt a <strong>contemporary teaching learning process</strong> with emphasis on hands on and collaborative learning.</span>
+  <span class="item">To facilitate <strong>skill development</strong> through additional training and encourage <strong>student forums</strong> for enhanced learning.</span>
+  <span class="item">To <strong>collaborate with industry partners and professional societies</strong> and make the students industry ready.</span>
+  <span class="item">To encourage innovation through <strong>multidisciplinary research and development activities.</strong></span>
+  <span class="item">To inculcate human values and ethics in students and groom them to be responsible citizens.</span>
+  </div><br><br>
+
+  <i style="font-size:15px;">The department Vision - Mission statements are formulated in accordance with the goals and objectives of the department keeping Institutional goals and objectives under consideration.</i><br><br>
+
+  <div>
+    <img src="images/HOD.jpg" style="border-radius:150px; width:150px; margin-left:50px; margin-right: 15px; display: inline; float: left;">
+    <p style="display: inline; font-size: 17px;">The Department of Computer Science and Engineering is positioned to create the technological leaders of tomorrow. Our goal is to ensure that our engineering graduates are well prepared to play the roles of problem solvers, project leaders, entrepreneurs, and above all ethical citizens of a global society. Our objective is to empower our students with latest technical knowledge and skills to meet global challenges. We provide them with the best teaching faculty, state-of-the-art technologies, excellent research and development environment and internships. Our aim is to provide professional grooming and guidance so that our students mould into thorough professionals and inspiring individuals when they enter into the real-time environment. <br><br>
+    <strong>Dr. Ramesh Babu D. R.</strong><br>
+    Vice-Principal,<br>
+    Professor & Head of the Department.</p> <br><br>
   </div>
-    </div>
+<a href="titles.php" style="font-size:20px; margin: 10px; float: left;" class="ui green button">List of Labs</a><br><br><br>
+
+  </div>
   <div class="ui inverted  large segment" >
     <div class="ui grid" style="" >
       <div class="five wide column" style="text-align:right ;">
       <h2 style="margin-top:15px;color:#365CCF;">
-       Under the Guidence of :
+       Under the Guidance of :
       </h2>
       <p style="color:#E9F035;">HOD of Computer Science Prof Ramesh Babu.</p>
       <p style="margin-left:20px;color:#E9F035;">Prof Anitha </p>
@@ -187,6 +93,7 @@
       </div>
 
     </div>
+  
   </div>
 </body>
 </html>
---
+++ titles.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Virtual Lab</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/Semantic/semantic.min.css">
+        <script src="css/Semantic/semantic.min.js"></script>
+        <script src="js/jquery-3.4.1.min.js"></script>
+        <link rel="stylesheet" href="css/home.css">
+        <title>Virtual-Labs</title>
+</head>
+<body>
+
+<h1 class="ui header" style="text-align:center" id = "head">
+      <a class="ui circular image" href="http://dsce.edu.in"><img src="https://cdn.glitch.com/f3f0a812-ef86-4316-8cee-84e24f8e112e%2Fdsce.png?v=1566407586257"></a>
+  Virtual Labs 
+</h1>
+<div id="toTop"><i class="chevron up icon"></i></div>
+  <div class="ui  light grey inverted secondary huge menu">
+  <a class="header item" href="index.php">
+    Home
+  </a>
+ <a class="item">
+    About Us
+  </a> 
+   <!-- <a class="item">
+    Item
+  </a>
+  <a class="item">
+    Item
+  </a> -->
+</div>
+</div>
+<div id="toTop"><i class="chevron up icon"></i></div>
+<div class="ui container" id="CardGrid">
+  <div class="ui stackable grid">
+    <div class="five wide column">
+      <div class="ui raised card" id="car1">
+       <div class="content" id="Card1">
+      <div class="header">
+            <a href="datastructures.php">
+             Data Structures Laboratory
+            </a></div>
+       </div>
+         <div class="content">
+           <span class="CardBody">
+             Contains 12 Experiments On Data Structures in C Programming Language
+           </span>
+           <br><br>
+           <a class="ui button" href="datastructures.php" >Go</a>
+         </div>
+      </div> 
+    </div>
+      
+    <div class="five wide column">
+        <div class="ui raised card" id="car2">
+       <div class="content" id="Card2">
+      <div class="header">
+         <a href="OopsWithJava.php" >
+             OOPS With Java Laboratory
+         </a>
+      </div>
+       </div>
+         <div class="content">
+           <span class="CardBody">
+             Contains 12 Experiments On Object Oriented Programming in Java
+           </span>
+           <br><br>
+           <a class="ui button" href="OopsWithJava.php" >Go</a>
+         </div>
+    </div>
+    </div>
+    <div class="five wide column">
+        <div class="ui  raised card" id="car3">
+       <div class="content" id="Card3">
+      <div class="header">
+         <a href="datastructures.php" >
+             Computer Networks Laboratory
+         </a>
+      </div>
+       </div>
+         <div class="content">
+           <span class="CardBody">
+             Contains 12 Experiments On Computer Networks in Cisco Packet Tracer
+           </span>
+           <br><br>
+           <a class="ui button" href="datastructures.php" >Go</a>
+         </div>
+  
+      </div>
+      </div>
+      
+      <div class="five wide column">
+        <div class="ui raised card" id="car4">
+       <div class="content" id="Card4">
+      <div class="header">
+         <a href="datastructures.php" >
+             Database Management Laboratory
+         </a>
+      </div>
+       </div>
+         <div class="content">
+           <span class="CardBody">
+             Contains 12 Experiments On Database Management in Oracle SQL
+           </span>
+           <br><br>
+           <a class="ui button" href="dbms.php" >Go</a>
+         </div>
+  
+      </div>
+      </div>
+  
+      <div class="five wide column">
+        <div class="ui raised card">
+       <div class="content" id="Card5">
+      <div class="header">
+         <a href="english.php" >
+             English Language
+         </a>
+      </div>
+       </div>
+         <div class="content">
+           <span class="CardBody">
+             Contains something related to the English Language
+           </span>
+           <br><br>
+           <a class="ui button" href="english.php">Go</a>
+         </div>
+  
+      </div>
+      </div>
+
+      <div class="five wide column">
+        <div class="ui raised card">
+       <div class="content" id="Card5">
+      <div class="header">
+         <a href="placement.php" >
+             Pre-Placement Training
+         </a>
+      </div>
+       </div>
+         <div class="content">
+           <span class="CardBody">
+             Contains something related to the Pre-placement stuffs
+           </span>
+           <br><br>
+           <a class="ui button" href="placement.php">Go</a>
+         </div>
+  
+      </div>
+      </div>
+      
+    
+  </div>
+    </div>
+  </div>
+  </div>
+</body>
+</html>
