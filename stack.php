4315b26942dee69b5122acdefe1dedd156470e59
Harshith R S <harshith.rs2008@gmail.com>
2019-09-13 11:48:11 +0530

    hello

--- stack.php
+++ stack.php
@@ -63,7 +63,7 @@
       <div id="int">
       Stack is a linear data structure which follows a particular order in which the operations are performed. The order may be LIFO(Last In First Out) or FILO(First In Last Out).
         <br><br>
-        <iframe style="width:100%; height:100%;" allowfullscreen="allowfullscreen" src="https://www.youtube.com/embed/sFVxsglODoo">
+        <iframe height="400px" width="100%" allowfullscreen="allowfullscreen" src="https://www.youtube.com/embed/sFVxsglODoo" frameborder="0">
 </iframe>
 <br><br>
 There are many real-life examples of a stack. Consider an example of plates stacked over one another in the canteen. The plate which is at the top is the first one to be removed, i.e. the plate which has been placed at the bottommost position remains in the stack for the longest period of time. So, it can be simply seen to follow LIFO(Last In First Out)/FILO(First In Last Out) order.
