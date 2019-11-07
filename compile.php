d795e9efe54e5d0e384a7c2f70b92dd19b66726b
Anush DP <anushdp.dsce@gmail.com>
2019-11-07 22:23:55 +0530

    Some huge commit

--- compile.php
+++ compile.php
@@ -1,4 +1,15 @@
 <?php
+if(session_status()==PHP_SESSION_NONE)
+{
+  session_start();
+  if(!isset($_SESSION['username']))
+  {
+    header("Location: index.php");
+    exit;
+  }
+}
+?>
+<?php
 
 	$languageID=$_POST["lang"];
     error_reporting(0);
