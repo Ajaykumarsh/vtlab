433aa82acb6a73c7aeacc796fab7b92771f63eb2
Anush DP <anushdp.dsce@gmail.com>
2019-11-07 22:37:06 +0530

    Added sql files

---
+++ tempfiles/sql/create table script.sql
@@ -0,0 +1,19 @@
+create table userdata ( 
+	username varchar(50),
+    semester varchar(6),
+	usn varchar(20) primary key,
+    mobile_no numeric(10) unique,
+    email varchar(20) unique,
+    passwd varchar(20)
+    );
+
+create table quiz ( question_no int unique , answer_no numeric(4));
+
+create table user_scores (
+	usn varchar(20),
+    quiz_score numeric(2),
+    program_score int,
+    quiz_no int unique,
+    program_no int unique,
+    primary key(usn, quiz_no, program_no),
+    foreign key(usn) references userdata(usn));
