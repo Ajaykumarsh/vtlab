bea4167fbe9715d46886547b3ffaa6789fb5c6a5
Anush DP <anushdp.dsce@gmail.com>
2019-11-09 17:43:46 +0530

    index page update

--- login.php
+++ login.php
@@ -4,6 +4,7 @@ if(session_status()==PHP_SESSION_NONE)
   session_start();
   if(isset($_SESSION['username']))
   {
+    $_SESSION['already_logged'] = "true";
     header("Location: titles.php");
     exit;
   }
