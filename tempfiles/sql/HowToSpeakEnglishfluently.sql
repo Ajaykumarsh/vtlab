16b092160660d11a5505cf6a23104cffc6672712
Anush DP <anushdp.dsce@gmail.com>
2019-11-09 23:05:11 +0530

    modded sql scripts

--- tempfiles/sql/HowToSpeakEnglishfluently.sql
+++ tempfiles/sql/HowToSpeakEnglishfluently.sql
@@ -1,26 +1,25 @@
-insert into answer_eng values ('en1a', 'English is everywhere. It’s in online videos, news channels, on radio. So listen and build your vocabulary.');
-insert into answer_eng values ('en1b', 'around');
-insert into answer_eng values ('en1c', 'between');
-insert into answer_eng values ('en1d', 'among');
+insert into answer_eng values ('hts1a', 'around');
+insert into answer_eng values ('hts1b', 'between');
+insert into answer_eng values ('hts1c', 'among');
+insert into answer_eng values ('hts1d', 'along');
 
-insert into answer_eng values ('en2a', 'along');
-insert into answer_eng values ('en2b', 'must have been dropping');
-insert into answer_eng values ('en2c', 'should have dropped');
-insert into answer_eng values ('en2d', 'must have dropped');
+insert into answer_eng values ('hts2a', 'must have been dropping');
+insert into answer_eng values ('hts2b', 'should have dropped');
+insert into answer_eng values ('hts2c', 'must have dropped');
+insert into answer_eng values ('hts2d', 'would have dropped');
 
-insert into answer_eng values ('en3a', 'would have dropped');
-insert into answer_eng values ('en3b', 'which');
-insert into answer_eng values ('en3c', 'that');
-insert into answer_eng values ('en3d', 'what');
+insert into answer_eng values ('hts3a', 'which');
+insert into answer_eng values ('hts3b', 'that');
+insert into answer_eng values ('hts3c', 'what');
+insert into answer_eng values ('hts3d', 'whom');
 
-insert into answer_eng values ('en4a', 'whom');
-insert into answer_eng values ('en4b', 'to read');
-insert into answer_eng values ('en4c', 'for reading');
-insert into answer_eng values ('en4d', 'reading');
+insert into answer_eng values ('hts4a', 'to read');
+insert into answer_eng values ('hts4b', 'for reading');
+insert into answer_eng values ('hts4c', 'reading');
+insert into answer_eng values ('hts4d', 'to have read');
 
-insert into answer_eng values ('en5a', 'to have read');
-insert into answer_eng values ('en5b', 'haven't ever');
-insert into answer_eng values ('en5c', 'haven't');
-insert into answer_eng values ('en5d', 'haven't got');
+insert into answer_eng values ('hts5a', 'haven\'t ever');
+insert into answer_eng values ('hts5b', 'haven\'t');
+insert into answer_eng values ('hts5c', 'haven\'t got');
+insert into answer_eng values ('hts5d', 'have');
 
-insert into answer_eng values ('en6a', 'have');
