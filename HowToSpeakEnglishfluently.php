860b8d7155bdc4da277a27af3bf32f8428925eca
Harshith R S <44067367+harshithrs@users.noreply.github.com>
2019-11-10 22:27:49 +0530

    Update HowToSpeakEnglishfluently.php

--- HowToSpeakEnglishfluently.php
+++ HowToSpeakEnglishfluently.php
@@ -40,6 +40,16 @@ if(session_status()==PHP_SESSION_NONE)
    <a class="item">
     About Us
   </a>
+    <?php
+    session_start();
+    if(!isset($_SESSION["username"])) echo "<a class=\"item\" href=\"login.php\">Login</a><a class=\"item\" href=\"register.php\">Register</a>";
+    else 
+    { 
+      echo "<a class=\"item\">"; 
+      echo $_SESSION['username'];
+      echo "</a><a class=\"item\" href=\"logoutprocess.php\">Logout</a>";
+    }
+    ?>
 </div>
 </div>
   <div class="ui container" id="cont">
